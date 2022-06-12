4c694784fc7f8f4d94fd791d69ae7cb11fc87eb0
Evgeniy Dainovich <zenbysel@gmail.com>
2022-06-12 13:33:52 +0300

    feat: add swagger docs

---
+++ docs/index.js
@@ -0,0 +1,14 @@
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const express = require("express");
+
+const app = express();
+
+const swaggerDocument = YAML.load(`${__dirname}/swagger.yml`);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(8000, () => {
+  console.log("Documentation is on http://localhost:8000/docs");
+});
+
