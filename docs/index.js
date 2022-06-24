3802bf000117b976b8c0ee5dba5dc37a326f3daa
Evgeniy Dainovich <zenbysel@gmail.com>
2022-06-24 17:04:40 +0300

    fix: add redirect to docs

--- docs/index.js
+++ docs/index.js
@@ -7,9 +7,7 @@ const app = express();
 const swaggerDocument = YAML.load(`${__dirname}/swagger.yml`);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/", (req, res) => {
-  return res.send('<a href="/docs">Link to docs</docs>');
-});
+app.use("/", (_, res) => res.redirect("/docs"));
 
 app.listen(8000, () => {
   console.log("Documentation is on http://localhost:8000/docs");
