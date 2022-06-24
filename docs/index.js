218b4f4ba30133d928aaba6f40047342f45ad5d9
Evgeniy Dainovich <zenbysel@gmail.com>
2022-06-24 17:03:35 +0300

    fix: add link to docs

--- docs/index.js
+++ docs/index.js
@@ -7,8 +7,10 @@ const app = express();
 const swaggerDocument = YAML.load(`${__dirname}/swagger.yml`);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", (req, res) => {
+  return res.send('<a href="/docs">Link to docs</docs>');
+});
 
 app.listen(8000, () => {
   console.log("Documentation is on http://localhost:8000/docs");
 });
-
