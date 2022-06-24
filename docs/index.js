015a3fc1bb9e8f547cc7631204a9a910048d3194
Evgeniy Dainovich <zenbysel@gmail.com>
2022-06-24 17:07:21 +0300

    fix: update port

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,8 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const express = require("express");
 
+const PORT = process.env.PORT || 8000;
+
 const app = express();
 
 const swaggerDocument = YAML.load(`${__dirname}/swagger.yml`);
@@ -9,6 +11,6 @@ const swaggerDocument = YAML.load(`${__dirname}/swagger.yml`);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", (_, res) => res.redirect("/docs"));
 
-app.listen(8000, () => {
-  console.log("Documentation is on http://localhost:8000/docs");
+app.listen(PORT, () => {
+  console.log(`Documentation is on http://localhost:${PORT}/docs`);
 });
