b60ab73c4b22b92feda8494e61586d9891918e4d
zenby <zenbysel@gmail.com>
2022-08-13 17:25:44 +0300

    fix: update deployment

--- src/bff-service/src/main.ts
+++ src/bff-service/src/main.ts
@@ -10,7 +10,7 @@ const corsOptions = {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
